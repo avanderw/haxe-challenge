1e7f0ddc53246becc8a59cd1020f72d32664a24a
Andrew van der Westhuizen <avanderw@gmail.com>
2018-03-08 16:39:05 +0200

    made a function of range check

--- src/net/avdw/MedianCut.hx
+++ src/net/avdw/MedianCut.hx
@@ -17,27 +17,52 @@ class MedianCut extends Sprite
 		var orig = Assets.getBitmapData("img/def611c3-b108-484f-85cc-38d208242101.jpg");
 		var origBmp = new Bitmap(orig);
 
-		var min = Color.fromRGB(255,255,255);
-		var max = Color.fromRGB(0,0,0);
+		var bucket:Array<Color> = new Array();
 		for (y in 0...orig.height)
 		{
 			for (x in 0...orig.width)
 			{
 				var pixel = Color.fromValue(orig.getPixel(x, y));
-				
-				min.r = cast Math.min(min.r, pixel.r);
-				min.g = cast Math.min(min.r, pixel.g);
-				min.b = cast Math.min(min.b, pixel.b);
-				
-				max.r = cast Math.max(max.r, pixel.r);
-				max.g = cast Math.max(max.g, pixel.g);
-				max.b = cast Math.max(max.b, pixel.b);
+				bucket.push(pixel);
 			}
 		}
-		
-		var range = Color.subtract(max, min);
-		
+
+		largestRange(bucket);
+
 		addChild(origBmp);
+	}
+
+	function largestRange(bucket:Array<Color>):String
+	{
+		var min = Color.fromRGB(255,255,255);
+		var max = Color.fromRGB(0,0,0);
+
+		for (color in bucket)
+		{
+			min.r = cast Math.min(min.r, color.r);
+			min.g = cast Math.min(min.r, color.g);
+			min.b = cast Math.min(min.b, color.b);
+
+			max.r = cast Math.max(max.r, color.r);
+			max.g = cast Math.max(max.g, color.g);
+			max.b = cast Math.max(max.b, color.b);
+		}
+
+		var range = Color.subtract(max, min);
+
+		if (range.r > range.g && range.r > range.b)
+		{
+			return "R";
+		}
+		else if (range.g > range.r && range.g > range.b)
+		{
+			return "G";
+		}
+		else if (range.b > range.r && range.b > range.g)
+		{
+			return "B";
+		}
+		return "R";
 	}
 
 }
